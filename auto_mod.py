6fbecbb2d75cb0a7c559a37cace2919fdcbb4ad6
listen2 <getifaddrs@gmail.com>
2012-08-21 14:25:04 -0400

    Implement comment rules.

--- auto_mod.py
+++ auto_mod.py
@@ -16,6 +16,7 @@ def log(lvl, s):
 class Rule:
     def __init__(self, name):
         self.rname = name
+        self.comment_type = False
 
     def __str__(self):
         return "<Rule(%s)>" % (self.rname)
@@ -48,6 +49,19 @@ class Rule:
             log(2, "REPORT %s" % (submission.permalink))
             #TODO
 
+class CommentRule(Rule):
+    def __init__(self, name):
+        super().__init__(name)
+        self.comment_type = True
+
+    def apply(self, comment):
+        if comment.subreddit.display_name not in self.reddits:
+            log(3, "SKIPPING %s %s" % (self.rname, comment.permalink))
+            return # Not all rules apply to all subreddits
+        if self.match(comment):
+            log(1, "MATCH %s: %s (%s)" % (self.rname, comment.permalink, comment.author))
+            self.do_actions(comment)
+
 class ImageRule(Rule):
     def match(self, submission):
         if submission.domain.lower() == "self." + self.rname.lower():
@@ -68,6 +82,9 @@ class UserRule(Rule):
     def match(self, submission):
         return self.re.match(submission.author.name)
 
+class CommentUserRule(CommentRule):
+    def match(self, comment):
+        return self.re.match(comment.author.name)
 
 class ButcherBot:
     class rule:
@@ -87,22 +104,36 @@ class ButcherBot:
                 rule = ImageRule(s)
             elif rtype == "title":
                 rule = TitleRule(s)
+            elif rtype == "comment_user":
+                rule = CommentUserRule(s)
             else:
                 rule = Rule(s) # This will probably cause a runtime error. Good.
             rule.re = re.compile(self.config.get(s, "re"))
             rule.comment = self.config.get(s, "comment")
             rule.distinguish = self.config.get(s, "distinguish").lower() in ["true", "1", "t", "y", "yes", "on"]
             rule.reddits = self.config.get(s, "reddits").split()
+            rule.actions = self.config.get(s, "actions").split()
             self.rules.append(rule)
             for sr in self.config.get(s, "reddits").split():
                 self.reddits.add(sr)
         log(3, "rules: %s" % (self.rules))
         log(3, "reddits: %s" % (self.reddits))
 
+        self.last_comment_time = self.config.getint("DEFAULT", "last_comment_time")
+
         # Log in
         self.r = praw.Reddit(user_agent=self.config.get("DEFAULT", "user_agent"))
         log(2, 'Logging in as %s...' % (self.config.get("DEFAULT", "user")))
         self.r.login(self.config.get("DEFAULT", "user"), self.config.get("DEFAULT", "pass"))
+
+        # Split comment and submission rules into separate lists for later efficiency
+        self.rules_submissions = []
+        self.rules_comments = []
+        for rule in self.rules:
+            if rule.comment_type == True:
+                self.rules_comments.append(rule)
+            else:
+                self.rules_submissions.append(rule)
 
 
     def save_config(self):
@@ -125,8 +156,24 @@ class ButcherBot:
                     log(2, "Post is already approved")
                     continue
 
-                for rule in self.rules:
+                for rule in self.rules_submissions:
                     rule.apply(submission)
+
+            first = True
+            if len(self.rules_comments) > 0:
+                log(3, "now processing comments")
+                for c in sub.get_comments(limit=1000):
+                    if first:
+                        #TODO can PRAW just give us the comments in chrono order?
+                        self.config.set("DEFAULT", "last_comment_time", str(c.created_utc))
+                        first = False
+                    if c.created_utc <= self.last_comment_time:
+                        log(3, "comment (%s) behind last_time (%s): %s" % (c.created_utc, self.last_comment_time, c))
+                        continue
+
+                    for rule in self.rules_comments:
+                        rule.apply(c)
+
 
         self.save_config()
 
